192e9a13303443a52d1c1f92c8942abd1c8f0a72
Jeffrey Yeh <jeffreyyehuw@gmail.com>
2023-01-17 19:39:47 -0800

    added JavaScript and README

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,15 @@
 // Assignment code here
-
+// user clicks generate password button
+  // addEventListener to button
+// prompt for user information regarding password preferences
+  // prompt for password length
+  // confirm character types to include
+  // if statement for # of falses
+// generate / display password
+  // make an array of possible choices for each character type
+  // if character type should be included (true), add to array
+  // for loop to randomly select from acceptable characters array (loop password length times)
+  // display password on page
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -11,6 +21,45 @@ function writePassword() {
 
   passwordText.value = password;
 
+}
+
+function generatePassword() {
+  var passwordLength = prompt("What is your desired password length?");
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert("Please choose a password length between 8 and 128 characters.");
+    return writePassword();
+  }
+  // console.log(passwordLength);
+  var lowerConfirm = confirm("Include lower case letters?");
+  var upperConfirm = confirm("Include upper case letters?");
+  var numberConfirm = confirm("Include numbers?");
+  var specialConfirm = confirm("Include special characters?");
+  // console.log(lowerConfirm);
+  // console.log(upperConfirm);
+  // console.log(numberConfirm);
+  // console.log(specialConfirm);
+  if (!lowerConfirm && !upperConfirm && !numberConfirm && !specialConfirm) {
+    alert("Please choose at least one character type.");
+    return writePassword();
+  }
+  var characters = [];
+  if (lowerConfirm) {
+    characters.push('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z');
+  }
+  if (upperConfirm) {
+    characters.push('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+  }
+  if (numberConfirm) {
+    characters.push('1', '2', '3', '4', '5', '6', '7', '8', '9', '0');
+  }
+  if (specialConfirm) {
+    characters.push('!', '@', '#', '$', '%', '^', '&', '*', '(', ')');
+  }
+  var password = '';
+  for (let i = 0; i < passwordLength; i++) {
+    password += characters[Math.floor(Math.random() * characters.length)];
+  }
+  return password;
 }
 
 // Add event listener to generate button
--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Password Generator Starter Code
+
+## Description
+
+This a website the allows the user to generate a password between 8 and 128 character in length and containing the types of characters they wish to include.
+
+## Installation
+
+N/A
+
+## Usage
+
+Click on the Generate Password button to generate a new password. Enter a number between 8 and 128 for the desired password length, then click OK. For each of the character types, click OK if you want it included in your password and click Cancel if you want it excluded. After answering all the questions, a randomly generated password will be displayed abiding by the user's preferences.
+
+## Link to deployed Application
+
+## Screenshot
+![alt=password-generator-website](Develop/screenshot.PNG);
+
+## Credits
+
+The creation of this website was guided by the UW Coding Bootcamp.
+
+## License
+
+N/A
